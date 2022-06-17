5c4e8d2001cbc97b157aa1eee0523f034700fcae
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-17 11:07:46 +0200

    tested 1st scenario of 1st app req

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <EventList />
+      </div>
+    );
   }
 }
 
