6f2be1e08ecb7dbcfc8dc0656465bd4984311264
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-18 11:31:39 +0200

    tested 3rd scenario of 1st app req

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -14,6 +14,12 @@ class CitySearch extends Component {
     this.setState({ query: value, suggestions });
   };
 
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+    });
+  };
+
   render() {
     return (
       <div className="CitySearch">
@@ -25,7 +31,12 @@ class CitySearch extends Component {
         />
         <ul className="suggestions">
           {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
           ))}
           <li key="all">
             <b>See all cities</b>
