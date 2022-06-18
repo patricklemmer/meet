5b30794cc2bd52769b91530ccf9f47a64e055a0e
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-18 23:34:41 +0200

    tested 2nd scenario for 3rd app req

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,14 +5,28 @@ class NumberOfEvents extends Component {
     numberOfEvents: 32,
   };
 
+  handleNumberChanged = (event) => {
+    const value = event.target.value;
+    if (!isNaN(value) && value >= 1) {
+      this.setState({ numberOfEvents: value });
+    } else {
+      console.log('Please enter a valid number.');
+      return this.state.numberOfEvents;
+    }
+    this.props.updateEvents(undefined, value);
+  };
+
   render() {
     return (
-      <div>
+      <div className="numberOfEvents">
+        <label htmlFor="events-number">Number of Events: </label>
+        <br />
         <input
           type="text"
           id="events-number"
           value={this.state.numberOfEvents}
-        ></input>
+          onChange={this.handleNumberChanged}
+        />
       </div>
     );
   }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,10 +5,10 @@ import NumberOfEvents from '../NumberOfEvents';
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
   });
 
-  test('render number input box', () => {
+  test('render input box', () => {
     expect(NumberOfEventsWrapper.find('#events-number')).toHaveLength(1);
   });
 
@@ -16,5 +16,28 @@ describe('<NumberOfEvents /> component', () => {
     expect(
       NumberOfEventsWrapper.find('#events-number').get(0).props.value
     ).toEqual(32);
+  });
+
+  test('user can change number of events', () => {
+    NumberOfEventsWrapper.find('#events-number').simulate('change', {
+      target: { value: 7 },
+    });
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(7);
+  });
+
+  test('allow number above 1 only', () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find('#events-number').simulate('change', {
+      target: { value: -1 },
+    });
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
+  });
+
+  test('allow input of numbers only', () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find('#events-number').simulate('change', {
+      target: { value: 'string' },
+    });
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
   });
 });
