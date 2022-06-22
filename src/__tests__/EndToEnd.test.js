764eec4dc0bcf1ca49c6961d3d4ce6def67f0c42
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-22 18:37:43 +0200

    end to end tested 1st app feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,5 +1,49 @@
 import puppeteer from 'puppeteer';
 
+// ------- First scope, 1st app feature
+describe('filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    // ({
+    //   headless: false,
+    //   slowMo: 250,
+    //   ignoreDefaultArgs: ['-disable-extensions'],
+    // });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('When a user hasnt searched for a city, show upcoming events from all cities', async () => {
+    await page.waitForSelector('.EventList');
+    const eventCount = (await page.$$('.event')).length;
+    expect(eventCount).toBe(2);
+  });
+
+  test('A user should see a list of suggestions when they search for a city', async () => {
+    await page.waitForSelector('.CitySearch');
+    await page.type('.city', 'Berlin');
+    const suggestionCount = (await page.$$('.suggestions li')).length;
+    expect(suggestionCount).toBe(2);
+  });
+
+  test('A user can select a city from the suggested list', async () => {
+    await page.reload();
+    await page.waitForSelector('.CitySearch');
+    await page.type('.city', 'Berlin');
+    await page.click('.suggestions li');
+    const eventCount = (await page.$$('.event')).length;
+    expect(eventCount).toBe(1);
+  });
+});
+
+// ------- Second scope, 2nd app feature
 describe('show/hide an event details', () => {
   let browser;
   let page;
