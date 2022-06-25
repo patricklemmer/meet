296587cda09d4b1745dde9befde5b441eefcc8ea
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-25 22:02:34 +0200

    added ErrorAlert to NumberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
   };
 
+  // handleNumberChanged = (event) => {
+  //   const value = event.target.value;
+  //   if (!isNaN(value) && value >= 1 && value <= 32) {
+  //     this.setState({ numberOfEvents: value });
+  //   }
+  //   this.props.updateEvents(undefined, value);
+  // };
+
   handleNumberChanged = (event) => {
     const value = event.target.value;
-    if (!isNaN(value) && value >= 1 && value <= 32) {
-      this.setState({ numberOfEvents: value });
+    this.props.updateEvents(null, value);
+    this.setState({ numberOfEvents: value });
+
+    if (value < 1) {
+      this.setState({
+        infoText: 'Select number from 1 to 32',
+      });
+    } else {
+      this.setState({
+        infoText: '',
+      });
     }
-    this.props.updateEvents(undefined, value);
   };
 
   render() {
