5e9e4067161606c802dbd630637f4c5a7a358706
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-27 21:31:05 +0200

    created and integrated OfflineAlert

--- src/Alert.js
+++ src/Alert.js
@@ -35,4 +35,11 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = `#FF8200`;
+  }
+}
+
+export { InfoAlert, ErrorAlert, OfflineAlert };
