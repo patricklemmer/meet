6523c043b5c950b1a84fe05524411d71cd38edce
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-27 21:31:25 +0200

    added and integrated privacy policy page

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import NProgress from 'nprogress';
 
 // Checks validity of access token && sends
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
