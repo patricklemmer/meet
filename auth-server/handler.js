6e0555d29ee63c9f2cff65a8e4e08514c66b3ecf
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-30 14:41:35 +0200

    changed redirect URL for testing

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://patricklemmer.github.io/meet/'],
+  redirect_uris: ['http://localhost:3000'],
   javascript_origins: [
     'https://patricklemmer.github.io',
     'http://localhost:3000',
