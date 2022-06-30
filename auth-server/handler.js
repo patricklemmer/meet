8edd9a6427d657145561eb2131b722ea5cd60f0d
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-30 16:12:19 +0200

    updated redirect URI

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,10 @@ const credentials = {
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
   redirect_uris: ['http://localhost:3000'],
-  javascript_origins: ['http://localhost:3000'],
+  javascript_origins: [
+    'https://patricklemmer.github.io',
+    'http://localhost:3000',
+  ],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
